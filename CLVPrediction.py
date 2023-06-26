f97612b14e438abe6bf316df6ba57ca0dbf80f0c
metoguzel <metehannguzel@gmail.com>
2023-06-26 14:38:27 +0300

    Update CLVPrediction.py
    data cleaning and transformation

--- CLVPrediction.py
+++ CLVPrediction.py
@@ -2,3 +2,33 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+
+df = pd.read_excel("Online Retail.xlsx")
+
+df.head()
+
+df.info()
+
+# drop null values
+
+df.dropna(subset="CustomerID", inplace=True)
+
+# keep positive values for Quantity and UnitPrice
+
+df = df.loc[df["Quantity"] > 0]
+
+df = df.loc[df["UnitPrice"] > 0]
+
+# ignore records for incomplete month
+
+df = df.loc[df["InvoiceDate"] < "2011-12-01"]
+
+# calculate total sales
+
+df["Sales"] = df["Quantity"] * df["UnitPrice"]
+
+# dataframe with orders
+
+df_orders = df.groupby(["CustomerID", "InvoiceNo"]).agg({"Sales": sum, "InvoiceDate": max})
+
+df_orders.head(8)
